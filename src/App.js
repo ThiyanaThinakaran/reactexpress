73639191e33b8d8cbfeb287e15525c0cfa7877bb
Thiyana T A <thiyana.ta@gavstech.com>
2023-10-27 19:55:03 +0530

    Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [userid, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const updateUserId = (event) => {setUserId(event.target.value);};
+
+  const updatePassword = (event) => {setPassword(event.target.value);};
+
+  const updateEmail = (event) => {setEmail(event.target.value);};
+
+  const insertUser = (event) => {
+    event.preventDefault();
+    axios.post('http://localhost:3000/insert', { userid: userid, password: password, emailID: email})
+      .then((res) => {console.log(res);
+        fetchData();
+      });
+  };
+
+  const updateuser = () => {
+    axios.put(`http://localhost:3000/update`, { userid: userid, password: password, emailID: email })
+      .then((res) => {
+        console.log(res);
+        fetchData();
+      });
+  };
+
+  const deleteuser = () => {
+    axios.delete(`http://localhost:3000/delete?userid=${userid}`)
+      .then((res) => {
+        console.log(res);
+        fetchData();
+      });
+  };
+
+  const fetchData = () => {
+    fetch('http://localhost:3000/getAll')
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <center>
+        <form onSubmit={insertUser}>
+          <b>User ID</b>
+          <input type="text" value={userid} onChange={updateUserId} /><br />
+          <b>Password</b>
+          <input type="password" value={password} onChange={updatePassword} /><br />
+          <b>Email ID</b>
+          <input type="email" value={email} onChange={updateEmail} /><br />
+          <input type="submit" value="Add" />&nbsp;&nbsp;
+          <input type="reset" value="Reset" />&nbsp;&nbsp;
+          <input type="button" value="Update" onClick={updateuser} />&nbsp;&nbsp;
+          <input type="button" value="Delete" onClick={deleteuser} />&nbsp;&nbsp;
+        </form>
+      </center>
+
+      <ul>
+        {data.map((item) => (
+          <li key={item.userid}>
+            {item.userid}, {item.password}, {item.emailID}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
